77f4555f536028d849f5e5a73084f54219516282
somto-arch <emmasomto459@gmail.com>
2024-07-14 18:18:49 +0100

    Update bar.js

--- bar.js
+++ bar.js
@@ -1,16 +1,19 @@
 let burgerBtn = document.querySelector('.burger-menu-btn');
 let burgerMenu = document.querySelector('.burger-menu');
-
+let img = document.querySelector('img');
 let isBurgerOpen = false;
 
 burgerBtn.onclick = function() {
     if (!isBurgerOpen) {
         burgerMenu.style.display = 'block';
+        img.style.display = 'none'
         burgerBtn.style.backgroundPosition = 'center left 50px, center';
         isBurgerOpen = true;
     }
     else if (isBurgerOpen) {
         burgerMenu.style.display = 'none';
+        img.style.display = 'block'
+        
         burgerBtn.style.backgroundPosition = 'center, center left 50px';
         isBurgerOpen = false;
     }
