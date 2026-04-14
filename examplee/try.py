e3c8b2e1985fa9a0f13a12f6577cf4ba04b93512
somto-arch <emmasomto459@gmail.com>
2026-04-14 13:33:21 +0100

    Update try.py

--- examplee/try.py
+++ examplee/try.py
@@ -1,3 +1,16 @@
+import os
+import getpass
+import socket
+
+# Get the current working directory (pwd)
+
+
 def pocdone():
-    for i in range(10):
-        print(i)
+    cwd = os.getcwd()
+    user = getpass.getuser()
+    machine = socket.gethostname()
+    print(f"Directory: {cwd}")
+    print(f"Username: {user}")
+    print(f"Machine:   {machine}")
+   # for i in range(10):
+      #  print(i)
