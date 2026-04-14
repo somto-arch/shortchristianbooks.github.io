a21b6838da0cb411f367749632e9885813dc88cd
somto-arch <emmasomto459@gmail.com>
2026-04-14 14:35:26 +0100

    Update try.py

--- examplee/try.py
+++ examplee/try.py
@@ -1,16 +1,16 @@
 import os
 
-def pocdone():
-    current_dir = os.getcwd() 
-        
-     while True:
+def poc_done():
+    current_dir = os.getcwd()
+    
+    while True:
         print(current_dir)
-            # Get parent directory
-         parent_dir = os.path.dirname(current_dir)
-            
-            # If the parent is the same as current, we've reached root
-         if parent_dir == current_dir:
-            break
-        current_dir = parent_dir
-     # Start at current working directory
         
+        # Get parent directory
+        parent_dir = os.path.dirname(current_dir)
+        
+        # If the parent is the same as current, we've reached root
+        if parent_dir == current_dir:
+            break
+            
+        current_dir = parent_dir
